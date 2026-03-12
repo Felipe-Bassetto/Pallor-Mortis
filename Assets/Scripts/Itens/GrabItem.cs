1cfa76d2e2d64cdd09b7973851b54574e7211720
Felipe Bassetto <felipe.bassetto2000@gmail.com>
2026-03-12 19:34:12 -0300

    Item na mão

--- Assets/Scripts/Itens/GrabItem.cs
+++ Assets/Scripts/Itens/GrabItem.cs
@@ -9,11 +9,15 @@ public class GrabItem : MonoBehaviour
 
     [Header("Item")]
     LayerMask layerMask;
+
+    [Header("Objetos")]
+    public ItensController it;
     // Start is called before the first frame update
     void Start()
     {
         cameraPrincipal = Camera.main;
         layerMask = LayerMask.GetMask("Item");
+        it = FindFirstObjectByType<ItensController>();
     }
 
     // Update is called once per frame
@@ -26,7 +30,7 @@ public class GrabItem : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
-
+                it.addItem(gameObject);
             } 
         }
     }
--- Assets/Scripts/Player/ItensController.cs
+++ Assets/Scripts/Player/ItensController.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class ItensController : MonoBehaviour
 {
     [Header("Itens")]
-    public GameObject[] arrItens = new GameObject[];
+    public List<GameObject> arrItens = new List<GameObject>();
+
+    private GameObject test;
 
     // Start is called before the first frame update
     void Start()
     {
-        addItem();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*If(Input.GetKeyDown(KeyCode.Q))
+        {
+            dropItem();
+        }*/
     }
 
-    public void addItem()
+    public void addItem(GameObject item)
     {
+        int qtdArr = arrItens.Count;
+        if(qtdArr < 2)
+        {
+            arrItens.Add(item);
+            test = item;
+        }
+    }
 
+    public void dropItem()
+    {
+        int qtdArr = arrItens.Count;
+        if(qtdArr > 0)
+        {
+            arrItens.Remove(test);
+        }
     }
 }
