5a8b6077e11d375000d90e2ece8049981f7670bc
Felipe Bassetto <felipe.bassetto2000@gmail.com>
2026-03-25 20:52:57 -0300

    nota voltando

--- Assets/Scripts/Itens/ReadNote.cs
+++ Assets/Scripts/Itens/ReadNote.cs
@@ -9,7 +9,9 @@ public class ReadNote : MonoBehaviour
     public float velocidadeRotacao = 1f;
 
     private Vector3 positionInitial;
+    private Quaternion rotationInitial;
     private bool noteMove = false;
+    private bool noteBack = false;
     private Vector3 noteReading = new Vector3(0,0,0.5f);
     private Quaternion noteRotation = Quaternion.Euler(-90, 0, 0);
 
@@ -38,7 +40,8 @@ public class ReadNote : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask) && !noteMove)
             {
-                positionInitial = gameObject.transform.position;
+                positionInitial = gameObject.transform.localPosition;
+                rotationInitial = gameObject.transform.rotation;
                 gameObject.transform.SetParent(cameraPrincipal.transform);
                 noteMove = true;
                 mov.PlayMovement(true);
@@ -46,12 +49,25 @@ public class ReadNote : MonoBehaviour
             } 
         }
 
-        if (noteMove) // Mantem a nota na tela para ser lida
+        if(noteMove) // Mantem a nota na tela para ser lida
         {
+            if(Input.GetKey(KeyCode.E)) // Voltar nota para posição original
+            {
+                gameObject.transform.SetParent(null);
+                noteBack = true;
+                noteMove = false;
+            }
+
             gameObject.transform.localPosition = Vector3.MoveTowards(gameObject.transform.localPosition, noteReading, velocidade * Time.unscaledDeltaTime);
             Quaternion lookRot = Quaternion.LookRotation(cameraPrincipal.transform.position - gameObject.transform.position, Vector3.up);
 
             gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation,lookRot,velocidadeRotacao * Time.unscaledDeltaTime);
+        }
+
+        if(noteBack)
+        {
+            gameObject.transform.localPosition = Vector3.MoveTowards(gameObject.transform.localPosition, positionInitial, velocidade * Time.unscaledDeltaTime);
+            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation,noteRotation,velocidadeRotacao * Time.unscaledDeltaTime);
         }
     }
 }
